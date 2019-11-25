941c1aed2f2e3987c63ba8bdead60ef07609c00e
Vinni Hoke <vinni.hoke@gmail.com>
2019-11-24 18:59:40 -0800

    Project Model completed, requires testing

--- routers/project-model.js
+++ routers/project-model.js
@@ -4,6 +4,31 @@ const find = () => {
 	return db('projects')
 }
 
+const findById = id => {
+	return db('projects').where({ id })
+}
+
+const findTasksById = id => {
+	return db('projects as p')
+		.select('p.id', 't.project_id', 't.description', 't.notes', 't.completed').innerJoin('tasks as t', function(){
+			this.on('p.id', '=', 't.project_id').andOn('p.id', '=', Number(id))
+		}).orderBy('t.project_id')
+}
+
+const add = project => {
+	return db('projects').insert(project).then(projects => {
+		return findById(projects[0])
+	})
+}
+
+const update = (id, changes) => {
+	return db('projects').where({ id }).update(changes);
+}
+
+const remove = id => {
+	return db('projects').where({ id }).del()
+}
+
 module.exports = {
-	find
+	find, findById, findTasksById, add, update, remove
 }
